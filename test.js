69f708ca930b61a2058dcaa8a76ac750ebc440c2
tunnckoCore <mameto_100@mail.bg>
2015-01-28 04:05:30 +0200

    update tests

--- test.js
+++ test.js
@@ -10,7 +10,7 @@
 var assert = require('assert');
 var cleanupCoverageCode = require('./index');
 
-describe('coverage-code-regex:', function() {
+describe('cleanup-coverage-code:', function() {
   it('should throw TypeError when not string given', function(done) {
     function fixture() {
       cleanupCoverageCode([1, 2, 3]);
@@ -24,6 +24,15 @@ describe('coverage-code-regex:', function() {
     var fixture = 'var a=[1,2,3];__cov_Ejgcx$XN18CSfmeWn$f7vQ.f[\'2\']++;var b=123;';
     var actual = cleanupCoverageCode(fixture);
     var expected = 'var a=[1,2,3];var b=123;';
+
+    assert.strictEqual(actual, expected);
+    done();
+  });
+
+  it('should return cleaned code, very big coverage code', function(done) {
+    var fixture = '__cov_jy8BOE3bkPEtewMs2GlPmg.f[\'58\']++;__cov_jy8BOE3bkPEtewMs2GlPmg.s[\'129\']++;assert.strictEqual(err,null);__cov_jy8BOE3bkPEtewMs2GlPmg.s[\'130\']++;assert.strictEqual(res[0],0);__cov_jy8BOE3bkPEtewMs2GlPmg.s[\'131\']++;assert.strictEqual(typeof res[1],\'string\');__cov_jy8BOE3bkPEtewMs2GlPmg.s[\'132\']++;assert(res[1].length>0);__cov_jy8BOE3bkPEtewMs2GlPmg.s[\'133\']++;done();';
+    var actual = cleanupCoverageCode(fixture);
+    var expected = 'assert.strictEqual(err,null);assert.strictEqual(res[0],0);assert.strictEqual(typeof res[1],\'string\');assert(res[1].length>0);done();';
 
     assert.strictEqual(actual, expected);
     done();
