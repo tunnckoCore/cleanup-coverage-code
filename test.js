0484beb674c38450bb2dfe2be282aeb1f4165318
tunnckoCore <mameto_100@mail.bg>
2015-01-28 05:30:52 +0200

    add test for complex coverage code

--- test.js
+++ test.js
@@ -38,6 +38,15 @@ describe('cleanup-coverage-code:', function() {
     done();
   });
 
+  it('should return cleaned code, when big and complex coverage code given', function(done) {
+    var fixture = '__cov_jy8BOE3bkPEtewMs2GlPmg.f[\'58\'][0]++;__cov_jy8BOE3bkPEtewMs2GlPmg.s[\'129\'][0][1]++;assert.strictEqual(err,null);__cov_jy8BOE3bkPEtewMs2GlPmg.s[\'130\'][1][3][0]++;assert.strictEqual(res[0],0);__cov_jy8BOE3bkPEtewMs2GlPmg.s[\'131\'][0][2][1][3]++;assert.strictEqual(typeof res[1],\'string\');__cov_jy8BOE3bkPEtewMs2GlPmg.s[\'132\'][1]++;assert(res[1].length>0);__cov_jy8BOE3bkPEtewMs2GlPmg.s[\'133\'][2]++;done();';
+    var actual = cleanupCoverageCode(fixture);
+    var expected = 'assert.strictEqual(err,null);assert.strictEqual(res[0],0);assert.strictEqual(typeof res[1],\'string\');assert(res[1].length>0);done();';
+
+    assert.strictEqual(actual, expected);
+    done();
+  });
+
   it('should return the given original code without change', function(done) {
     var fixture = 'var a=[1,2,3];var b=123;';
     var actual = cleanupCoverageCode(fixture);
